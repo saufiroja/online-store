7567b640d2d79f012e8fc967fdd070fccfc2ce54
saufiroja <saufiroja08@gmail.com>
2022-08-04 09:30:05 +0800

    feat: model database

--- entity/user_entity.go
+++ entity/user_entity.go
@@ -1,12 +1,30 @@
 package entity
 
-type User struct {
-	Id       string `json:"id"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+import (
+	"time"
 
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
-	DeletedAt string `json:"deleted_at"`
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+// create role
+type Role string
+
+const (
+	RoleAdmin Role = "admin"
+	RoleUser  Role = "user"
+)
+
+type User struct {
+	Id         uuid.UUID `json:"id" gorm:"primary_key;not null"`
+	Name       string    `json:"name" gorm:"not null" validate:"required"`
+	Email      string    `json:"email" gorm:"not null;unique" validate:"required,email"`
+	Password   string    `json:"password" gorm:"not null" validate:"required,min=8,max=100"`
+	Role       Role      `json:"role" gorm:"not null"`
+	Otp        string    `json:"otp" gorm:"not null"`
+	OtpExpired int64     `json:"otp_expiry" gorm:"not null"`
+
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at"`
 }
