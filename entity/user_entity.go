7394821054421c7a62b0d9897a23f8e367beb201
saufiroja <saufiroja08@gmail.com>
2022-08-04 16:20:03 +0800

    docs: update default role

--- entity/user_entity.go
+++ entity/user_entity.go
@@ -3,7 +3,6 @@ package entity
 import (
 	"time"
 
-	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
@@ -16,13 +15,13 @@ const (
 )
 
 type User struct {
-	Id         uuid.UUID `json:"id" gorm:"primary_key;not null"`
-	Name       string    `json:"name" gorm:"not null" validate:"required"`
-	Email      string    `json:"email" gorm:"not null;unique" validate:"required,email"`
-	Password   string    `json:"password" gorm:"not null" validate:"required,min=8,max=100"`
-	Role       Role      `json:"role" gorm:"not null"`
-	Otp        string    `json:"otp" gorm:"not null"`
-	OtpExpired int64     `json:"otp_expiry" gorm:"not null"`
+	Id         string `json:"id" gorm:"primary_key;not null"`
+	Name       string `json:"name" gorm:"not null" validate:"required"`
+	Email      string `json:"email" gorm:"not null;unique" validate:"required,email"`
+	Password   string `json:"password" gorm:"not null" validate:"required,min=8,max=100"`
+	Role       Role   `json:"role" gorm:"not null;default:'user'"`
+	Otp        string `json:"otp" gorm:"not null"`
+	OtpExpired int64  `json:"otp_expiry" gorm:"not null"`
 
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
