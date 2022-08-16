30d9996ac6c876ff34fa622de84db43d37951e3b
saufiroja <saufiroja08@gmail.com>
2022-08-16 17:25:38 +0700

    docs: add image column

--- entity/product_entity.go
+++ entity/product_entity.go
@@ -12,7 +12,10 @@ type Product struct {
 	Description string `json:"description" gorm:"type:text;not null"`
 	Stock       int    `json:"stock" gorm:"type:int;not null"`
 	Price       int    `json:"price" gorm:"type:int;not null"`
-	UserId      string `json:"user_id" gorm:"not null"`
+	Image       string `json:"image" gorm:"type:varchar(255);not null"`
+	UserId      string `json:"user_id" gorm:"type:varchar(255);not null"`
+
+	User User `json:"-" gorm:"foreignkey:UserId"`
 
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
