f9c63972d95766ad85b97068787bf1ca05e60c59
saufiroja <saufiroja08@gmail.com>
2022-08-17 16:57:53 +0700

    feat: add category

--- config/db.go
+++ config/db.go
@@ -37,7 +37,7 @@ func IntiDB(conf Config) *gorm.DB {
 
 	db, err := gorm.Open(postgres.Open(dns), &gorm.Config{})
 
-	db.AutoMigrate(&entity.User{}, &entity.Role{}, &entity.Product{})
+	db.AutoMigrate(&entity.User{}, &entity.Role{}, &entity.Category{}, &entity.Product{})
 
 	if err != nil {
 		log.Fatal(err)
